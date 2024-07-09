0ab47599bcca06a62779a35ced9e9166779290ad
ayussh203 <139635900+p-singh9950@users.noreply.github.com>
2024-07-09 13:33:07 +0530

    update

--- portfloioo/src/components/NavBar.jsx
+++ portfloioo/src/components/NavBar.jsx
@@ -4,14 +4,20 @@ import logo from '../assets/Ay.png'; // Ensure the path to the logo is correct
 
 const Navbar = () => {
   return (
-    <nav className="mb-20 flex items-center justify-between py--6">
+    <nav className="mb-20 flex items-center justify-between py-6">
       <div className="flex flex-shrink-0 items-center">
-        <img className="mx-2 w-10 "src={logo} alt="logo" />
+        <img className="mx-2 w-10" src={logo} alt="logo" />
       </div>
       <div className="m-8 flex items-center justify-center gap-4 text-2xl">
-        <FaLinkedin />
-        <FaGithub />
-        <FaInstagram />
+        <a href="https://www.linkedin.com/in/ayush-srivastava-2b793621b/" target="_blank" rel="noopener noreferrer">
+          <FaLinkedin />
+        </a>
+        <a href="https://github.com/ayussh203" target="_blank" rel="noopener noreferrer">
+          <FaGithub />
+        </a>
+        <a href="https://www.instagram.com/ayush_sriv?igsh=MXAybWJqMTFoZWZpag==" target="_blank" rel="noopener noreferrer">
+          <FaInstagram />
+        </a>
       </div>
     </nav>
   );
